1f5066d638799f4de0cd2358427cb35610379b69
boyuan-oops <848242097@qq.com>
2020-11-14 15:36:16 +0800

    homework——1

---
+++ demo1.py
@@ -0,0 +1,31 @@
+"""
+一个回合制游戏，每个角色都有hp和power，
+hp代表血量，power代表攻击力，hp的初始值为1000，
+power的初始值为200。打斗多个回合
+定义一个fight方法：
+my_hp = hp - enemy_power
+enemy_final_hp = enemy_hp - my_power
+谁的hp先为0，那么谁就输了
+"""
+
+hp = 1000
+def fight():
+    ##定义四个变量分别代表各自血量和攻击力
+    my_hp = 500
+    my_power = 200
+    enemy_hp = 200
+    enemy_power = 200
+    while True:
+        my_hp = hp - enemy_power
+        enemy_final_hp = enemy_hp - my_power
+        if(my_hp == 0 & enemy_final_hp == 0):
+            print("平局")
+            break
+        elif(my_hp ==0):
+            print("我输了")
+            break
+        elif(enemy_final_hp == 0):
+            print("敌人输了")
+            break
+
+fight()
